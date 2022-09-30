76047d0df19d7c79cf620c17f3d3aeec0787c4f8
NarekAleksanian <76249305+NarekAleksanian@users.noreply.github.com>
2022-09-30 16:45:59 +0400

    Update reverseInParentheses.py

--- day4/reverseInParentheses.py
+++ day4/reverseInParentheses.py
@@ -1,6 +1,2 @@
 def solution(inputString):
-    end = inputString.find(")")
-    start = inputString.rfind("(", 0, end)
-    if end == -1:
-        return inputString
-    return solution(inputString[:start] + inputString[start+1:end][::-1] + inputString[end+1:])
+    return (n*n)+((n-1)*(n-1))
